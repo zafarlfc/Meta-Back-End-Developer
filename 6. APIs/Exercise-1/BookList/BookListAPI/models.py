c5eb9053fcf3d45578baa05ea3a0c8c81a476109
zafarlfc <zafarlfc@outlook.com>
2024-07-13 23:51:08 +0500

    create model schema for Books

--- 6. APIs/Exercise-1/BookList/BookListAPI/models.py
+++ 6. APIs/Exercise-1/BookList/BookListAPI/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    price = models.DecimalField(decimal_places=2)
+
+    def __str__(self) -> str:
+        return self.title
