f350ae6339835e8e845b0668fa2cb0d58e82ae2f
zafarlfc <zafarlfc@outlook.com>
2024-07-14 23:55:01 +0500

    added rest framework settings in settings.py file

--- 6. APIs/Exercise-2/LittleLemon/LittleLemon/settings.py
+++ 6. APIs/Exercise-2/LittleLemon/LittleLemon/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'LittleLemonDRF'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+        "rest_framework_xml.renderers.XMLRenderer",
+    ]
+}
