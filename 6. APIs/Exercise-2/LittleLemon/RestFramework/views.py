2a8b9dec85b879beb6aa143f20350d55430032a5
zafarlfc <zafarlfc@outlook.com>
2024-07-15 23:49:00 +0500

    added class based views for menu items

--- 6. APIs/Exercise-2/LittleLemon/RestFramework/views.py
+++ 6. APIs/Exercise-2/LittleLemon/RestFramework/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from .models import MenuItems
+from .serializers import MenuItemSerializer
 
 # Create your views here.
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItems.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItems.objects.all()
+    serializer_class = MenuItemSerializer
