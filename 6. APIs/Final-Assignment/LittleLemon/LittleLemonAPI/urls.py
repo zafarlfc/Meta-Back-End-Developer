be0bf3179e85584c82fde083aadf17a6c5b5732c
zafarlfc <zafarlfc@outlook.com>
2024-08-08 23:30:01 +0500

    Added url pattern for cart view

--- 6. APIs/Final-Assignment/LittleLemon/LittleLemonAPI/urls.py
+++ 6. APIs/Final-Assignment/LittleLemon/LittleLemonAPI/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("categories", views.CategoriesView.as_view()),
     path("menu-items", views.MenuItemsView.as_view()),
-    path("menu-items/<int:pk>", views.SingleMenuItemView.as_view())
+    path("menu-items/<int:pk>", views.SingleMenuItemView.as_view()),
+    path("cart/menu-items", views.CartView.as_view())
 ]
