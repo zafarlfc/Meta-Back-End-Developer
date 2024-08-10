6e16ec4d1ad63ac61887e8176e58a9dcf5452f16
zafarlfc <zafarlfc@outlook.com>
2024-08-10 23:47:30 +0500

    added url pattern for order view

--- 6. APIs/Final-Assignment/LittleLemon/LittleLemonAPI/urls.py
+++ 6. APIs/Final-Assignment/LittleLemon/LittleLemonAPI/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("categories", views.CategoriesView.as_view()),
     path("menu-items", views.MenuItemsView.as_view()),
     path("menu-items/<int:pk>", views.SingleMenuItemView.as_view()),
-    path("cart/menu-items", views.CartView.as_view())
+    path("cart/menu-items", views.CartView.as_view()),
+    path("orders", views.OrderView.as_view()),
 ]
