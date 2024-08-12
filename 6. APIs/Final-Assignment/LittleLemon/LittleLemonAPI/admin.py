d3d0fa7e22bd747bf08bd6e2b3b0545781816862
zafarlfc <zafarlfc@outlook.com>
2024-08-12 16:36:54 +0500

    registered models in admin file

--- 6. APIs/Final-Assignment/LittleLemon/LittleLemonAPI/admin.py
+++ 6. APIs/Final-Assignment/LittleLemon/LittleLemonAPI/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Category)
+admin.site.register(MenuItem)
+admin.site.register(Cart)
+admin.site.register(Order)
+admin.site.register(OrderItem)
